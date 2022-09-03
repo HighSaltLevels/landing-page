012836c5cb7cc96d47eed0a687c7c217c2c0325c
David Greeson <swdrummer13@gmail.com>
2022-09-03 13:02:40 -0400

    Use a golang pod reader instead of python
    Not only is golang more performant here, it's safer to use golang
    as the web driver because the default python web driver is not very
    good without something like SGI.

---
+++ podreader/cmd/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"podreader/formatter"
+	"podreader/readers"
+	"strings"
+)
+
+func getStatus(w http.ResponseWriter, r *http.Request, reader *readers.Reader) {
+	// Remove all "/" from the url in an attempt to get the
+	// desired namesapce. There shouldn't be any "/" other than
+	// the preceding and ending "/" so any request with more
+	// than those 2 uses of "/" are invalid anyway and will
+	// return a 404.
+	namespace := strings.Replace(r.URL.Path, "/", "", -1)
+	log.Printf("Recevied request for namespace: %s", namespace)
+	status, code := reader.GetStatus(namespace)
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// If we're returning an error, just write the error directly
+	if code != 200 {
+		w.WriteHeader(code)
+		io.WriteString(w, status)
+		return
+	}
+
+	formattedStatus, err := formatter.FormatStatus(status)
+	if err != nil {
+		w.WriteHeader(500)
+		io.WriteString(w, formattedStatus)
+		return
+	}
+	io.WriteString(w, formattedStatus)
+}
+
+func main() {
+	reader := readers.NewReader()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		getStatus(w, r, reader)
+	})
+
+	log.Print("Pod Reader starting...")
+	if err := http.ListenAndServe(":42069", nil); err != nil {
+		log.Fatalf("Got error: %v\n", err)
+	}
+}
---
+++ podreader/go.mod
@@ -0,0 +1,5 @@
+module podreader
+
+go 1.13
+
+require k8s.io/api v0.25.0
